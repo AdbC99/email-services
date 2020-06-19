f9267a303d404c51ecdbbde2e39e339ef8993174
AdbC99 <alistair_de_b_clarkson@yahoo.co.uk>
2020-06-19 12:37:23 +1000

    Added express swagger

--- app.js
+++ app.js
@@ -9,6 +9,37 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// setup swagger
+const expressSwagger = require('express-swagger-generator')(app);
+
+let options = {
+  swaggerDefinition: {
+      info: {
+          description: 'This is a sample server',
+          title: 'Swagger',
+          version: '1.0.0',
+      },
+      host: 'localhost:3000',
+      basePath: '/v1',
+      produces: [
+          "application/json",
+          "application/xml"
+      ],
+      schemes: ['http', 'https'],
+      securityDefinitions: {
+          JWT: {
+              type: 'apiKey',
+              in: 'header',
+              name: 'Authorization',
+              description: "",
+          }
+      }
+  },
+  basedir: __dirname, //app absolute path
+  files: ['./routes/**/*.js'] //Path to the API handle folder
+};
+expressSwagger(options)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
